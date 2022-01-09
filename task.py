39da4951a049e41dc8caadcff852e2fc6b599515
Jay <jayaramakrishnan0421@gmail.com>
2022-01-09 14:26:06 +0000

    task file

---
+++ task.py
@@ -0,0 +1,20 @@
+#Task - 1
+
+#Get values from the user for weight and height and calculate BMI.  
+
+#BMI = weight / height^2
+
+
+
+#Task - 2
+
+# Average age = 90
+# Get the value from the user for age and caculate their balance life in years, months, weeks and days
+
+
+#   Task - 3
+
+# Get the bill amount and tip they wish to give (10, 15, 20) from the customer then calculate the total bill
+
+
+
